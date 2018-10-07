0bb093f653f004fc193734f6619fd9befd5a3769
NitinBuzz <reachnitinvenkat@gmail.com>
2018-10-08 02:55:58 +0530

    Updates

---
+++ precache-manifest.86ec3cea8a0a663a6b01848655625578.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e88d550c8f1cc2c724d4",
+    "url": "/diagnal.react.wokrshop/static/js/runtime~main.e88d550c.js"
+  },
+  {
+    "revision": "e3a1f38cf0ba2896a3be",
+    "url": "/diagnal.react.wokrshop/static/js/main.e3a1f38c.chunk.js"
+  },
+  {
+    "revision": "d53c567b1f36087ce805",
+    "url": "/diagnal.react.wokrshop/static/js/1.d53c567b.chunk.js"
+  },
+  {
+    "revision": "53167bc1066707bfcffacdc59beddab7",
+    "url": "/diagnal.react.wokrshop/index.html"
+  }
+];
